30610344a09b1bcbecbc68eef9188fdef67f03a5
Mohamed Montfort <mohamedmontfort2003@gmail.com>
2024-12-09 19:55:30 +0100

    details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Element } from "react-scroll";
-import FormContact from "./form-contact.jsx";
 import "../styles/Contact.css";
+import { FaArrowDown } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <Element name="contact" className="contact-section">
       <h2>Me contacter</h2>
-      <FormContact />
+      <div className="contact-text">
+        <p>
+          Mon profil vous intéresse ? N'hésitez pas à me contacter directement
+          par e-mail !
+        </p>
+        <FaArrowDown className="arrow-icon" />
+        <span>mohamedmontfort2003@gmail.com</span>
+      </div>
     </Element>
   );
 }
